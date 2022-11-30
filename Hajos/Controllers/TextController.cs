60e9f328a04aae3e23b2444b3123900d94ee2deb
ctikgt <ctikgt@SZA0030895>
2022-11-30 12:35:22 +0100

    Add project files.

---
+++ Hajos/Controllers/TextController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hajos.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class TextController : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult M1()
+        {
+            string pontosIdő = DateTime.Now.ToShortTimeString();
+
+            return Ok(pontosIdő);
+        }
+
+        [HttpGet]
+        [Route("corvinus/nagybetus/{szoveg}")]
+        public IActionResult M2(string szoveg)
+        {
+            return Ok(szoveg.ToUpper());
+            
+        }
+
+
+    }
+    
+}
